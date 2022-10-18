871e5d054e965ccbb2423181a5fc679918f76b36
Emanuel Aguirre Molina <emagui11572@gmail.com>
2022-10-17 19:54:27 -0500

    Api de Pokemon

---
+++ src/components/Pokemon/PokemonButton.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import styles from "./PokemonButton.module.css";
+
+
+const Pokemon = () =>{
+    const [list, setList] = useState([])
+    const [appear, setAppear] = useState(false)
+
+    const showlist = (e) =>{
+        e.preventDefault();
+        appear === false?
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+            .then(response => response.json())
+            .then(response => setList(response.results))
+        :setList([])
+        appear === false?setAppear(true):setAppear(false);
+        console.log(appear);
+    }
+
+    return(
+        <>
+        <button onClick={showlist} className={styles.button}>Fetch Pokemon</button>
+        <ol>
+        {list !== []?
+        list.map((pokemon,num) => { return (<li key={num}>{num+1}. {pokemon.name}</li>)}):
+        null}
+        </ol>
+        </>
+    )
+}
+
+export default Pokemon;
